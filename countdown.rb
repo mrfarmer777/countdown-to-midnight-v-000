be5b10c1cbe9920be39c0dbee92d03df970dca5f
Matt Farmer <mrfarmer777@gmail.com>
2018-05-25 21:05:38 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -12,9 +12,9 @@ end
 def countdown_with_sleep(integer)
   count=integer
   while count>-1
-    sleep(1)
     puts "#{count} SECOND(S)!"
     count-=1
+    sleep(1)
   end
   return "HAPPY NEW YEAR!"
 end
